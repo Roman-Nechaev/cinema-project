a02495de749cf4df0d70ae38b5e3c391d0fca036
Roman-Nechaev <nrchaevroman@gmail.com>
2023-05-17 00:21:44 +0300

    fix log

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-export const WrapperPagination = styled.div`
-  /* display: flex; */
-`;
+export const WrapperPagination = styled.div``;
 
 export const Tittle = styled.h1`
   text-align: center;
