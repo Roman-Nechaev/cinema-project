2d7fe1f37dcca797ddd8babfabff57f7a9a7f72e
Roman-Nechaev <nrchaevroman@gmail.com>
2023-05-24 13:19:34 +0300

    TESTING:baseUrl

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/Roman-Nechaev/cinema-project/settings/pages',
+  base: '/https://github.com/Roman-Nechaev/cinema-project',
 });
